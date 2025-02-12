04500903745c2041c2f66fc04b3a903140318278
H-chai <143179912+H-chai@users.noreply.github.com>
2025-02-12 10:25:48 +0100

    Add CSS Modules styling for layout components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
 import { Nav } from "./Nav";
-import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import styles from "../styles/Header.module.css";
+import Logo from "../logo.svg";
 
 export function Header() {
   return (
-    <header>
-      <button>
-        <MenuOutlinedIcon></MenuOutlinedIcon>
-      </button>
-      <Link to="/"></Link>
+    <header className={styles.headerElements}>
+      <Link to="/">
+        <img src={Logo} alt="Logo" />
+      </Link>
       <Nav />
     </header>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,25 +2,32 @@ import { NavLink } from "react-router-dom";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import styles from "../styles/Nav.module.css";
 
 export function Nav() {
   return (
     <nav>
-      <ul>
-        <li>
-          <NavLink>
+      <ul className={styles.headerNavList}>
+        <li className={styles.navListItem}>
+          <NavLink className={styles.navListItem}>
             <PersonOutlineOutlinedIcon></PersonOutlineOutlinedIcon>
           </NavLink>
         </li>
-        <li>
-          <NavLink>
+        <li className={styles.navListItem}>
+          <NavLink className={styles.navListItem}>
             <FavoriteBorderOutlinedIcon></FavoriteBorderOutlinedIcon>
           </NavLink>
         </li>
-        <li>
-          <NavLink>
+        <li className={styles.navListItem}>
+          <NavLink className={styles.navListItem}>
             <ShoppingCartOutlinedIcon></ShoppingCartOutlinedIcon>
           </NavLink>
+        </li>
+        <li>
+          <button className={styles.navButton}>
+            <MenuOutlinedIcon></MenuOutlinedIcon>
+          </button>
         </li>
       </ul>
     </nav>
