c77f929ef071174ab3487e3a0743e03579c578bd
H-chai <143179912+H-chai@users.noreply.github.com>
2025-02-21 12:32:50 +0100

    get product data by using api hook

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,3 +1,19 @@
+import { useParams } from "react-router-dom";
+import { useApi } from "../hooks/useApi";
+
 export function Product() {
-  return <div>Single Product</div>;
+  let { id } = useParams();
+  const { data, isLoading, isError } = useApi(
+    `https://v2.api.noroff.dev/online-shop/${id}`
+  );
+
+  if (isLoading) {
+    return <div>loading</div>;
+  }
+
+  if (isError) {
+    return <div>Error: {isError}</div>;
+  }
+
+  return <div>Single Product {data.id}</div>;
 }
