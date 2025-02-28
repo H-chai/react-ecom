5f4961cbbcc66962e514a552886e255b3eebe72f
H-chai <143179912+H-chai@users.noreply.github.com>
2025-02-28 20:48:54 +0100

    When cart is empty, display message. When any item is in cart, display the item(s)

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -16,42 +16,53 @@ export function Cart() {
     <div>
       <h2>Cart</h2>
       <div>
-        {cart.map((product) => (
-          <div key={product.id}>
-            <figure>
-              <img src={product.image.url} alt={product.image.alt} />
-            </figure>
-            <p>{product.title}</p>
-            <div>
-              <div>
-                <p>
-                  {product.price === product.discountedPrice
-                    ? ""
-                    : `$${product.price}`}
-                </p>
-                <p>${product.discountedPrice}</p>
-              </div>
-              <div>
-                <button onClick={() => subtractQuantity(product.id)}>
-                  <RemoveIcon></RemoveIcon>
-                </button>
-                <p>{product.quantity}</p>
-                <button onClick={() => addQuantity(product.id)}>
-                  <AddIcon></AddIcon>
-                </button>
-              </div>
-            </div>
-            <button onClick={() => removeFromCart(product.id)}>
-              <CloseIcon></CloseIcon>
-            </button>
+        {cart.length === 0 ? (
+          <div>
+            <h3>Your shopping cart is empty.</h3>
+            <p>
+              When you add products to your shopping bag, they will appear here.
+            </p>
           </div>
-        ))}
+        ) : (
+          <div>
+            {cart.map((product) => (
+              <div key={product.id}>
+                <figure>
+                  <img src={product.image.url} alt={product.image.alt} />
+                </figure>
+                <p>{product.title}</p>
+                <div>
+                  <div>
+                    <p>
+                      {product.price === product.discountedPrice
+                        ? ""
+                        : `$${product.price}`}
+                    </p>
+                    <p>${product.discountedPrice}</p>
+                  </div>
+                  <div>
+                    <button onClick={() => subtractQuantity(product.id)}>
+                      <RemoveIcon></RemoveIcon>
+                    </button>
+                    <p>{product.quantity}</p>
+                    <button onClick={() => addQuantity(product.id)}>
+                      <AddIcon></AddIcon>
+                    </button>
+                  </div>
+                </div>
+                <button onClick={() => removeFromCart(product.id)}>
+                  <CloseIcon></CloseIcon>
+                </button>
+              </div>
+            ))}
+            <div>
+              <p>Total</p>
+              <p>${totalCost.toFixed(2)}</p>
+            </div>
+            <button>Checkout</button>
+          </div>
+        )}
       </div>
-      <div>
-        <p>Total</p>
-        <p>${totalCost.toFixed(2)}</p>
-      </div>
-      <button>Checkout</button>
     </div>
   );
 }
