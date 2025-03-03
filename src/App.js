5e42dd01ce10e0c644090d3277a689d7b24ac97c
H-chai <143179912+H-chai@users.noreply.github.com>
2025-03-03 19:55:18 +0100

    Deleted unused files and images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
 import { Layout } from "./components/Layout";
 import { Home } from "./pages/Home";
 import { Product } from "./pages/Product";
